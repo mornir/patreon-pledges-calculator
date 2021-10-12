6e86868d2c597d2383d262ae58ff3a58a6873b81
mornir <jeromepott@hotmail.com>
2021-10-12 19:30:16 +0200

    feat: add link to creator's patreon page closes #11

--- src/components/Creator.vue
+++ src/components/Creator.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
+    <!--  alt="" is better for screen readers since the name is already stated below-->
     <img
       :src="photo"
       alt=""
       class="w-24 h-24 mx-auto border-2 rounded-full border-light-gray"
     />
-    <p class="mt-2 text-lg font-medium text-center text-secondary">
-      {{ name }}
-    </p>
+
+    <a
+      :href="url"
+      target="_blank"
+      class="block mt-2 text-lg font-medium text-center text-secondary hover:underline"
+      >{{ name }}</a
+    >
+
     <p class="text-xl font-medium text-center">{{ formatPledge(pledged) }}</p>
   </div>
 </template>
@@ -29,6 +35,11 @@ export default {
     pledged: {
       type: Number,
       default: 0,
+      required: true,
+    },
+    url: {
+      type: String,
+      default: "https://www.patreon.com",
       required: true,
     },
   },
